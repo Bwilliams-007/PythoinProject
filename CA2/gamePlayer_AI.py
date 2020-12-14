4638f8b819f081b09ec62d84c9299798f245a857
Bwilliams-007 <Babajidewilliamss@gmail.com>
2020-12-14 14:09:45 +0000

    Humanplayerfunction

--- CA2/gamePlayer_AI.py
+++ CA2/gamePlayer_AI.py
@@ -1,5 +1,5 @@
 '''
-   This is a build of different class Gameplayer class, Humanely class and the AI class. 
+   This is a build of different class Gameplayer class, Humanely class and the AI class.
 '''
 
 import math
@@ -14,3 +14,17 @@ class gamePlayer:
 class HumanPlayer(gamePlayer):
     def __init__(self, let):
         super().__init__(let)
+
+    def get_move(self, game):
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + '\'s move. Enter (0-9) to make a move: ')
+            try:
+                val = int(square)
+                if val not in game.a_moves():
+                    raise ValueError
+                valid_square = True
+            except ValueError:
+                print('Invalid square. Try again.')
+        return val
